09233bfc9008ea03656bbbd3de290654be4fb9d2
Katleho Mokhethi <katlehomokhethi1@gmail.com>
2022-04-11 18:45:51 +0200

    React Components  Part 2

---
+++ ReactProjects/my-app/src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+        <img className='header_logo' 
+            src=''>
+
+        </img>
+    </div>
+  )
+}
+
+export default Header
---
+++ ReactProjects/my-app/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDkmpor2t4lZXVB7SImdffv_M5XFYr7m98",
+    authDomain: "challenge-2faf3.firebaseapp.com",
+    projectId: "challenge-2faf3",
+    storageBucket: "challenge-2faf3.appspot.com",
+    messagingSenderId: "755456393070",
+    appId: "1:755456393070:web:de06e155c17f9664b74b7f",
+    measurementId: "G-MJYFSR9HHY"
+  };
